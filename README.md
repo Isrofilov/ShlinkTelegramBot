0fc655eda38113392c821795543b1c7b9cc5355b
Artyom Isrofilov <artyom@isrofilov.ru>
2024-02-26 22:01:11 +0300

    add QR code sending and refactor code

--- README.md
+++ README.md
@@ -24,18 +24,16 @@ To set up the ShlinkTelegramBot service, you need to create a `docker-compose.ym
           TELEGRAM_BOT_TOKEN: your_telegram_bot_token_here
           SHLINK_API_TOKEN: your_shlink_api_token_here
           SHLINK_SERVER_URL: your_shlink_server_url_here
-          ALLOWED_TELEGRAM_IDS: "123456789,987654321"
-
-
+          #ALLOWED_TELEGRAM_IDS: "123456789,987654321"
+          #SEND_QR_CODE: true
 
 Here's what you need to replace:
 
 - `your_telegram_bot_token_here`: Replace this with your actual Telegram bot token.
 - `your_shlink_api_token_here`: Replace this with your Shlink API token.
 - `your_shlink_server_url_here`: Replace this with the full URL of your Shlink server, including `https://`. For example, `https://example.com`.
-- `ALLOWED_TELEGRAM_IDS`: These are the Telegram user IDs that are allowed to interact with the bot. You can list multiple IDs separated by commas.
-
-Make sure to use the correct URL format and include `https://` to ensure secure communication with the Shlink server.
+- `ALLOWED_TELEGRAM_IDS`(optional): These are the Telegram user IDs that are allowed to interact with the bot. You can list multiple IDs separated by commas. If this variable is not set, the bot will respond to any user.
+- `SEND_QR_CODE` (optional): Set this to `true` if you want the bot to send a QR code along with the short link. If not set or set to `false`, the bot will only send the short link.
 
 ### Running the Bot
 
--- shlink.py
+++ shlink.py
@@ -6,7 +6,16 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 SHLINK_API_TOKEN = os.getenv('SHLINK_API_TOKEN')
 SHLINK_SERVER_URL = os.getenv('SHLINK_SERVER_URL')
-ALLOWED_TELEGRAM_IDS = set(int(x) for x in os.getenv('ALLOWED_TELEGRAM_IDS', '').split(','))
+ALLOWED_TELEGRAM_IDS = os.getenv('ALLOWED_TELEGRAM_IDS')
+SEND_QR_CODE = os.getenv('SEND_QR_CODE', 'false').lower() == 'true'
+
+if not TELEGRAM_BOT_TOKEN or not SHLINK_API_TOKEN or not SHLINK_SERVER_URL:
+    raise EnvironmentError('Missing one or more required environment variables.')
+
+if ALLOWED_TELEGRAM_IDS:
+    ALLOWED_TELEGRAM_IDS = set(int(x) for x in ALLOWED_TELEGRAM_IDS.split(','))
+else:
+    ALLOWED_TELEGRAM_IDS = None
 
 async def start(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
@@ -18,21 +27,43 @@ async def start(update: Update, context: CallbackContext) -> None:
 def shorten_url(url: str) -> str:
     headers = {'X-Api-Key': f'{SHLINK_API_TOKEN}'}
     data = {'longUrl': url}
-    response = requests.post(f'{SHLINK_SERVER_URL}/rest/v2/short-urls', headers=headers, json=data)
+    response = requests.post(f'{SHLINK_SERVER_URL}/rest/v3/short-urls', headers=headers, json=data)
     if response.status_code == 200:
-        short_url = response.json().get('shortUrl', '')
-        return short_url
+        short_url_data = response.json()
+        short_url = short_url_data.get('shortUrl', '')
+        short_code = short_url_data.get('shortCode', '')
+        return short_url, short_code
     else:
-        return 'Error when shortening a link.'
+        return 'Error when shortening a link.', None
+
+def generate_qr_code(short_code: str) -> (bytes, str):
+    if short_code is None:
+        return None, 'Error when generating QR code because short link was not created.'
+    response = requests.get(f'{SHLINK_SERVER_URL}/{short_code}/qr-code')
+    if response.status_code == 200:
+        return response.content, None
+    else:
+        return None, 'Error when generating QR code.'
 
 async def handle_message(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
-    if user_id not in ALLOWED_TELEGRAM_IDS:
+    if ALLOWED_TELEGRAM_IDS is not None and user_id not in ALLOWED_TELEGRAM_IDS:
         await update.message.reply_text('You do not have access to this bot.')
         return
     url = update.message.text
-    short_url = shorten_url(url)
-    await update.message.reply_text(short_url)
+    short_url, short_code = shorten_url(url)
+    if short_url.startswith('Error'):
+        await update.message.reply_text(short_url)
+        return
+    if SEND_QR_CODE:
+        qr_code_bytes, error_message = generate_qr_code(short_code)
+        if error_message:
+            await update.message.reply_text(short_url)
+            await update.message.reply_text(error_message)
+        else:
+            await context.bot.send_photo(chat_id=update.effective_chat.id, photo=qr_code_bytes, caption=short_url)
+    else:
+        await update.message.reply_text(short_url)
 
 def main() -> None:
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
